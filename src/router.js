b61cde3e76e2adb477d863226b6bebef398c34ca
bean-baek <106383499+bean-baek@users.noreply.github.com>
2024-11-22 22:44:17 +0900

    📝 docs: 파일 수정

--- src/router.js
+++ src/router.js
@@ -1,25 +1,4 @@
-import Home from '@pages/Home';
-import Login from '@pages/Login';
-import Profile from '@pages/Profile';
-import Game from '@pages/game/Game';
-import CreateRoom from '@pages/game/CreateRoom';
-import JoinRoom from '@pages/game/JoinRoom';
-import WaitingRoom from '@pages/game/WaitingRoom';
-import Friends from '@pages/Friends';
-import PlayGame from '@pages/game/PlayGame';
-
-
-class Router {
-    static routes = {
-      '/': Home,
-      '/login': Login,
-      '/profile': Profile,
-      '/game': Game,
-      '/game/create': CreateRoom,
-      '/game/join': JoinRoom,
-      '/game/waiting': WaitingRoom,
-      '/game/play': PlayGame,
-      '/friends': Friends,
-    };
-  }
-  
+import Home from '@pages/home';
+import Login from '@pages/login';
+import Profile from '@pages/profile';
+import Game from '@pages/game/game';
