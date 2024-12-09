da87ced3e82da9a6386a14f0d4b8cd4cf9ae4673
Woorim Kim <wrkimlimi@gmail.com>
2024-12-09 20:20:35 +0900

    :sparkles: feat: 토큰 필요한 요청을 위한 autoFetch 작성

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,69 +1,69 @@
 const USER_API_URL = process.env.USER_API_URL;
 
 export const checkNickname = async (nickname) => {
-    const response = await fetch(`${USER_API_URL}/check-nickname/`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ nickname }),
-    });
+  const response = await fetch(`${USER_API_URL}/check-nickname/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ nickname }),
+  });
 
-    const data = await response.json();
-    if (!response.ok) {
-        throw new Error(data.message);
-    }
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
 
-    return data;
-}
+  return data;
+};
 
 export const registerUser = async (userData) => {
-    const response = await fetch(`${USER_API_URL}/register/`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-    });
+  const response = await fetch(`${USER_API_URL}/register/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
 
-    const data = await response.json();
-    if (!response.ok) {
-        throw new Error(data.message || "Failed to register user");
-    }
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || 'Failed to register user');
+  }
 
-    return data;
+  return data;
 };
 
 export const verifyEmail = async (userData) => {
-    const response = await fetch(`${USER_API_URL}/verify-email/`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-    });
+  const response = await fetch(`${USER_API_URL}/verify-email/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
 
-    const data = await response.json();
-    if (!response.ok) {
-        throw new Error(data.message || "Failed to verify email");
-    }
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || 'Failed to verify email');
+  }
 
-    return data;
+  return data;
 };
 
 export const loginUser = async (userData) => {
-    const response = await fetch(`${USER_API_URL}/login/`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-    });
+  const response = await fetch(`${USER_API_URL}/login/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
 
-    const data = await response.json();
-    if (!response.ok) {
-        throw new Error(data.message || "Failed to login user");
-    }
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || 'Failed to login user');
+  }
 
-    return data;
-}
+  return data;
+};
