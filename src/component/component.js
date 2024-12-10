c5323b4717db2e92ea2e253582c0b23c5a160154
Woorim Kim <wrkimlimi@gmail.com>
2024-12-10 17:00:23 +0900

    :bug: fix: virtual DOM 비 슷하게 도입

--- src/component/component.js
+++ src/component/component.js
@@ -12,13 +12,13 @@ export default class Component {
     this.$props = $props;
     this.setup();
     this.render();
+    this.setEvent();
   }
 
   setup() {} //컴포넌트 state 설정
 
   mounted() {
     console.log(`${this.constructor.name} mounted.`);
-    this.setEvent();
   } //컴포넌트가 마운트 되었을 때
 
   template() {
@@ -36,7 +36,78 @@ export default class Component {
   setState(newState) {
     //상태 변경 후 렌더링
     this.$state = { ...this.$state, ...newState };
+
+    // 가상 DOM 생성 (현재 DOM 복사)
+    const oldNode = this.$target.cloneNode(true);
+
+    // 새로운 상태로 렌더링
     this.render();
+
+    // 실제 변경된 부분만 업데이트
+    this.updateDOM(this.$target, oldNode);
+  }
+
+  updateDOM(newNode, oldNode) {
+    // 노드 타입이 다르면 교체
+    if (
+      newNode.nodeType !== oldNode.nodeType ||
+      newNode.tagName !== oldNode.tagName
+    ) {
+      oldNode.parentNode?.replaceChild(newNode, oldNode);
+      return;
+    }
+
+    // input, select, textarea의 값 보존
+    if (
+      oldNode instanceof HTMLInputElement ||
+      oldNode instanceof HTMLSelectElement ||
+      oldNode instanceof HTMLTextAreaElement
+    ) {
+      const oldValue = oldNode.value;
+      const oldChecked = oldNode.checked;
+      const newValue = newNode.value;
+      const newChecked = newNode.checked;
+
+      if (newValue !== oldValue) {
+        newNode.value = oldValue;
+      }
+      if (newChecked !== oldChecked) {
+        newNode.checked = oldChecked;
+      }
+    }
+
+    // 텍스트 노드면 내용만 업데이트
+    if (newNode.nodeType === Node.TEXT_NODE) {
+      if (newNode.textContent !== oldNode.textContent) {
+        oldNode.textContent = newNode.textContent;
+      }
+      return;
+    }
+
+    // 속성 업데이트
+    if (newNode.attributes && oldNode.attributes) {
+      [...newNode.attributes].forEach((attr) => {
+        const oldAttr = oldNode.getAttribute(attr.name);
+        if (oldAttr !== attr.value) {
+          oldNode.setAttribute(attr.name, attr.value);
+        }
+      });
+    }
+
+    // 자식 노드들 재귀적으로 비교
+    const newChildren = [...newNode.childNodes];
+    const oldChildren = [...oldNode.childNodes];
+    const maxLen = Math.max(newChildren.length, oldChildren.length);
+
+    for (let i = 0; i < maxLen; i++) {
+      if (!oldChildren[i]) {
+        oldNode.appendChild(newChildren[i].cloneNode(true));
+      } else if (!newChildren[i]) {
+        oldNode.removeChild(oldChildren[i]);
+      } else {
+        this.updateDOM(newChildren[i], oldChildren[i]);
+      }
+    }
   }
 
   addEvent(eventType, selector, callback) {
