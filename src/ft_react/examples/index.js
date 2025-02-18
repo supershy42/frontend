267051b100e68ae2e7a0e985df1ccf5246f1adc2
Woorim Kim <wrkimlimi@gmail.com>
2025-02-18 21:54:26 +0900

    :green_heart: chore: add test page

--- src/ft_react/examples/index.js
+++ src/ft_react/examples/index.js
@@ -4,12 +4,45 @@ import { createRoot } from '../supereact-dom/index.js';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-const element = jsx('div', {
-  className: 'test',
-  children: [
-    jsx('h1', { children: 'Hello World' }),
-    jsx('p', { children: 'This is a test' }),
-  ],
-});
+const element = {
+  type: 'div',
+  props: {
+    children: [
+      {
+        type: 'h1',
+        props: { children: 'Counter: 0' },
+      },
+      {
+        type: 'button',
+        props: {
+          onclick: () => {
+            // 업데이트 테스트
+            root.render({
+              type: 'div',
+              props: {
+                children: [
+                  {
+                    type: 'h1',
+                    props: { children: 'Counter: 1' },
+                  },
+                  {
+                    type: 'button',
+                    props: {
+                      onclick: () => {
+                        /* ... */
+                      },
+                      children: 'Increment',
+                    },
+                  },
+                ],
+              },
+            });
+          },
+          children: 'Increment',
+        },
+      },
+    ],
+  },
+};
 
 root.render(element);
